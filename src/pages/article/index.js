6a12fd40a22792ccc5e87d94e850e8f69d673706
humingxian <hudadacool@163.com>
2019-03-12 20:04:26 +0800

    1

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
 class Article extends Component{
-
-
-
+  constructor(props) {
+    super(props)
+    this.myRef = React.createRef()
+    
+  }
+  componentDidMount() {
+    this.myRef.current.focus()
+    this.myRef.current.value = 12
+  }
 
   render() {
+    console.log(this.myRef)
     return (
       <div>
         <img src={require('../../assert/images/ggx.png')} alt='' />
+        <input ref={this.myRef}></input>
       </div>
     )
   }
